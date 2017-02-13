799b155de38d19a0f46d31f9020628b2e7fa9cbd
omar zarate <alberich_sid@hotmail.com>
2017-02-13 11:54:28 -0600

    Se creo el modelo de Persona

---
+++ app/Persona.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace sisVentas;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Persona extends Model
+{
+	//Hacemos ferencia ala tabla persona
+    protected $table='persona';
+    //hacemos refencia al id de persona
+    protected $primaryKey='idpersona';
+
+    public $timestamps=false;
+    //le decimos que campos podemos acceder
+    protected $fillable =[
+     'tipo_persona',
+     'nombre',
+     'tipo_documento',
+     'num_documento',
+     'direccion',
+     'telefono',
+     'email'
+    ];
+
+    protected $guarded =[
+
+    ];
+}

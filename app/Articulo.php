12d8dee655b341f253f459ea30eef93d502dea30
omar zarate <alberich_sid@hotmail.com>
2017-02-13 10:41:48 -0600

    Se creo el modelo ycontrolador de Articulos con todas sus funciones del index,create,edit,update,show y destroy

---
+++ app/Articulo.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace sisVentas;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Articulo extends Model
+{
+	//hacemos referencia al tabla que vamos a manejar
+    protected $table='articulo';
+    //se defin ela llave primaria de la tabla articulo
+    protected $primaryKey='idarticulo';
+
+    public $timestamps=false;
+
+    protected $fillable =[
+    	'idcategoria',
+    	'codigo',
+    	'nombre',
+    	'stock',
+    	'descripcion',
+    	'imagen',
+    	'estado'
+    ];
+
+    protected $guarded =[
+
+    ];
+}
---
+++ app/Http/Controllers/ArticuloController.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace sisVentas\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use sisVentas\Http\Requests;
+
+use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Facades\Input;
+use sisVentas\Http\Requests\ArticuloFormRequest;
+use sisVentas\Articulo;
+use DB;
+
+class ArticuloController extends Controller
+{
+    //construtor
+    public function __construct()
+    {
+
+    }
+    //funcion index
+    public function index(Request $request)
+    {
+        if ($request)
+        {
+            //filtro de busquedas
+            $query=trim($request->get('searchText'));
+            //condicion
+            $articulos=DB::table('articulo as a')
+            ->join('categoria as c','a.idcategoria','=','c.idcategoria')
+            ->select('a.idarticulo','a.nombre','a.codigo','a.stock','c.nombre as categoria','a.descripcion','a.imagen','a.estado')
+            ->where('a.nombre','LIKE','%'.$query.'%')
+            ->orderBy('a.idarticulo','desc')
+            ->paginate(7);
+            return view('almacen.articulo.index',["articulos"=>$articulos,"searchText"=>$query]);
+        }
+    }
+    //Funcion para la vista de crear un nuevo articulo
+    public function create()
+    {
+    	$categorias=DB::table('categoria')->where('condicion','=','1')->get();
+        //mostrar vista de crear
+        return view("almacen.articulo.create",["categorias"=>$categorias]);
+    }
+    //funcion para insertar un nuevo articulo
+    public function store (ArticuloFormRequest $request)
+    {
+        //creo objeto de validacion tipo categoria
+        $articulo=new Articulo;
+        $articulo->idcategoria=$request->get('idcategoria');
+        $articulo->codigo=$request->get('codigo');
+        $articulo->nombre=$request->get('nombre');
+        $articulo->stock=$request->get('stock');
+        $articulo->descripcion=$request->get('descripcion');
+        $articulo->estado='Activo';
+
+        if (Input::hasFile('imagen')){
+        	$file=Input::file('imagen');
+        	$file->move(public_path().'/imagenes/articulos/',$file->getClientOriginalName();
+        		$articulo->imagen=$file->getClientOriginalName();
+        }
+
+        $articulo->save();
+        return Redirect::to('almacen/articulo');
+
+    }
+
+    //funcion para mostrar todos los articulos
+    public function show($id)
+    {
+        return view("almacen.articulo.show",["articulo"=>Articulo::findOrFail($id)]);
+    }
+
+    //funciojn para editar cada articulo
+    public function edit($id)
+    {
+        $articulo=Articulo::findOrFail($id);
+        $categorias=DB::table('categoria')->where('condicion','=','1')->get();
+        //retornar la vista
+        return view("almacen.articulo.edit",["articulo"=>$articulo,"categorias"=>$categorias]);
+
+    }
+
+    //funcion para actualizar la informacion de cada articulo
+    public function update(ArticuloFormRequest $request,$id)
+    {
+         $articulo=Articulo::findOrFail($id);
+         $articulo->idcategoria=$request->get('idcategoria');
+         $articulo->codigo=$request->get('codigo');
+         $articulo->nombre=$request->get('nombre');
+         $articulo->stock=$request->get('stock');
+         $articulo->descripcion=$request->get('descripcion');
+         $articulo->estado='Activo';
+
+         if (Input::hasFile('imagen')){
+         	$file=Input::file('imagen');
+         	$file->move(public_path().'/imagenes/articulos/',$file->getClientOriginalName());
+        	$articulo->imagen=$file->getClientOriginalName();
+        }
+
+         $articulo->update();
+         return Redirect::to('almacen/articulo');
+    }
+
+    //funcion para eliminar cada articulo
+    public function destroy($id)
+    {
+        $articulo=Articulo::findOrFail($id);
+        $articulo->Estado='Inactivo';
+        $articulo->update();
+         return Redirect::to('almacen/articulo');
+    }
+}

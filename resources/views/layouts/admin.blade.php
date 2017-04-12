a7bcb1aba9ffd15eaf1d6797134fd87f2b75b53c
omar zarate <alberich_sid@hotmail.com>
2017-04-12 11:31:23 -0500

    se agregaron las rutas de la plantilla principal layout llamada admin.blade

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -117,8 +117,8 @@
                  <i class="fa fa-angle-left pull-right"></i>
               </a>
               <ul class="treeview-menu">
-                <li><a href="ventas/venta"><i class="fa fa-circle-o"></i> Ventas</a></li>
-                <li><a href="ventas/cliente"><i class="fa fa-circle-o"></i> Clientes</a></li>
+                <li><a href="{{ url('/ventas/venta') }}"><i class="fa fa-circle-o"></i> Ventas</a></li>
+                <li><a href="{{ url('/ventas/cliente') }}"><i class="fa fa-circle-o"></i> Clientes</a></li>
               </ul>
             </li>
                        

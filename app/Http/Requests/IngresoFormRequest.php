71285647cbfc7ff2be74459289190ef2bb46f5f5
omar zarate <alberich_sid@hotmail.com>
2017-04-12 10:12:23 -0500

    se volvio agregar las reglqas form request ingreso

--- app/Http/Requests/IngresoFormRequest.php
+++ app/Http/Requests/IngresoFormRequest.php
@@ -25,7 +25,13 @@ class IngresoFormRequest extends Request
     {
         return [
             
-                       
+            'tipo_comprobante'=>'required|max:20',
+            'serie_comprobante'=>'max:7',
+            'num_comprobante'=>'required|max:10',
+            'idarticulo'=>'required',
+            'cantidad'=>'required',
+            'precio_compra'=>'required',
+            
         ];
     }
 }

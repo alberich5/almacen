2f624c43af15a4227832e9ab4ad041afde1ebf4e
omar zarate <alberich_sid@hotmail.com>
2017-04-18 14:29:00 -0500

    cambios para le generacion de pdf en el proyecto

--- resources/views/vista.blade.php
+++ resources/views/vista.blade.php
@@ -5,7 +5,7 @@
     <title>Plantilla de descarga</title>
     <link rel="stylesheet" href="css/style.css" media="all" />
   </head>
-  <body>
+  <body leftmargin="0" topmargin="0" marginwidth="0" marginheight="0" rightmargin="0">
 
     <header class="clearfix">
       <div id="logo">
@@ -34,73 +34,9 @@
       </div>
 
         <div id="cabeza">
-            <table border="1">
-          <thead>
-            <tr>
-              <th>Clave</th>
-              <th>dias</th>
-            </tr>
-          </thead>
-          
-        </table><br><br>  
+         <p>prueba</p>
         </div>  
-      <table border="1" cellspacing="0" cellpadding="0">
-        <thead>
-          <tr>
-            <th class="no">#</th>
-            <th class="desc">DESCRIPTION</th>
-            <th class="unit">UNIT PRICE</th>
-            <th class="qty">QUANTITY</th>
-            <th class="total">TOTAL</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td class="no">01</td>
-            <td class="desc"><h3>Website Design</h3>Creating a recognizable design solution based on the company's existing visual identity</td>
-            <td class="unit">$40.00</td>
-            <td class="qty">30</td>
-            <td class="total">$1,200.00</td>
-          </tr>
-          <tr>
-            <td class="no">02</td>
-            <td class="desc"><h3>Website Development</h3>Developing a Content Management System-based Website</td>
-            <td class="unit">$40.00</td>
-            <td class="qty">80</td>
-            <td class="total">$3,200.00</td>
-          </tr>
-          <tr>
-            <td class="no">03</td>
-            <td class="desc"><h3>Search Engines Optimization</h3>Optimize the site for search engines (SEO)</td>
-            <td class="unit">$40.00</td>
-            <td class="qty">20</td>
-            <td class="total">$800.00</td>
-          </tr>
-        </tbody>
-        <tfoot>
-          <tr>
-            <td colspan="2"></td>
-            <td colspan="2">SUBTOTAL</td>
-            <td>$5,200.00</td>
-          </tr>
-          <tr>
-            <td colspan="2"></td>
-            <td colspan="2">TAX 25%</td>
-            <td>$1,300.00</td>
-          </tr>
-          <tr>
-            <td colspan="2"></td>
-            <td colspan="2">GRAND TOTAL</td>
-            <td>$6,500.00</td>
-          </tr>
-        </tfoot>
-      </table>
-      <div id="thanks">Gracias</div>
-      <div id="notices">
-        <div>NOTICE:</div>
-        <div class="notice">A finance charge of 1.5% will be made on unpaid balances after 30 days.</div>
-      </div>
-    </main>
+      
 
     <footer>
       aqui va la parte de firmas

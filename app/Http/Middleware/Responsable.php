9996ecef005967dfbfa29a3a0f3189c357349dca
Omar Zarate Hipolito <alberich_sid@hotmail.com>
2017-07-04 11:55:59 -0500

    se creo otro middleware para para ekl rol responsav

--- app/Http/Middleware/Responsable.php
+++ app/Http/Middleware/Responsable.php
@@ -14,8 +14,26 @@ class Responsable
      * @param  \Closure  $next
      * @return mixed
      */
+    public function __construct(Guard $auth)
+    {
+        $this->auth=$auth;
+    }
+
     public function handle($request, Closure $next)
     {
+        switch ($this->auth->user()->idrol) {
+           case '1':
+               # Administrador
+                return  redirect()->to('admin');
+               break;
+            case '2':
+               # Responsable de agregar productos
+                #return  redirect()->to('responsable');
+               break;
+           
+           
+       }
+
         return $next($request);
     }
 }

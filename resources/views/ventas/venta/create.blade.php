e6b9b9705c7323b55db2813cc08e42f582755cfb
Omar Zarate Hipolito <alberich_sid@hotmail.com>
2017-07-10 09:39:51 -0500

    se esta realizando pruebas sobre el co0orrecto control de las informacion de la de stock

--- resources/views/ventas/venta/create.blade.php
+++ resources/views/ventas/venta/create.blade.php
@@ -165,7 +165,6 @@ function mostrarValores(){
    {
 
     if(stock>=cantidad){
-        alert("Estas dentro del if para comprobar que funcione bien");
         subtotal[cont]=(cantidad*precio_venta);
         total=total+subtotal[cont];
         var fila='<tr class="selected" id="fila'+cont+'"><td><button type="button" class="btn btn-warning" onclick="eliminar('+cont+')">x</button></td><td><input type="hidden" name="idarticulo[]" value="'+idarticulo+'">'+articulo+'</td><td><input type="number"  name="cantidad[]" value="'+cantidad+'"></td><td><input type="number"  name="precio_venta[]" value="'+precio_venta+'"></td><td>'+subtotal[cont]+'</td></tr>';
